47549ac12b63a7b7ea189c4d1e2ab2ce28ca64aa
Pablo Vannini <vanninipablo@gmail.com>
2019-08-14 22:47:12 -0300

    sword added

--- src/main/java/com/frienddementor/obsidiantools/init/ModItems.java
+++ src/main/java/com/frienddementor/obsidiantools/init/ModItems.java
@@ -4,13 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.frienddementor.obsidiantools.items.ItemBase;
+import com.frienddementor.obsidiantools.items.tools.ToolSword;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems {
 
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
+	// Materials
+	public static final ToolMaterial MATERIAL_OBSIDIAN = EnumHelper.addToolMaterial(
+			"material_obsidian",
+			ToolMaterial.IRON.getHarvestLevel(),
+			ToolMaterial.DIAMOND.getMaxUses(),
+			ToolMaterial.IRON.getEfficiency(),
+			ToolMaterial.IRON.getAttackDamage(),
+			ToolMaterial.IRON.getEnchantability()
+	);
+	
+	// Items
 	public static final Item OBSIDIAN_INGOT = new ItemBase("obsidian_ingot");
 
+	// Tools
+	public static final ItemSword OBSIDIAN_SWORD = new ToolSword("obsidian_sword", MATERIAL_OBSIDIAN);
 }
---
+++ src/main/java/com/frienddementor/obsidiantools/items/tools/ToolSword.java
@@ -0,0 +1,26 @@
+package com.frienddementor.obsidiantools.items.tools;
+
+import com.frienddementor.obsidiantools.Main;
+import com.frienddementor.obsidiantools.init.ModItems;
+import com.frienddementor.obsidiantools.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSword;
+
+public class ToolSword extends ItemSword implements IHasModel{
+
+	public ToolSword(String name, ToolMaterial material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MATERIALS);
+		
+		ModItems.ITEMS.add(this);
+	}
+	@Override
+	public void registerModels() {
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+		
+	}
+}
