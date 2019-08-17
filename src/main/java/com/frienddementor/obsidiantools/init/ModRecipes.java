277a1eb26f458fbfa5cd4bb33f8201db2129646e
Pablo Vannini <vanninipablo@gmail.com>
2019-08-17 01:21:48 -0300

    add smelting recipes

--- src/main/java/com/frienddementor/obsidiantools/init/ModRecipes.java
+++ src/main/java/com/frienddementor/obsidiantools/init/ModRecipes.java
@@ -5,8 +5,21 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
+	static float ITEM_TO_NUGGET_EXP = 3f;
+
 	public static void init()
 	{
 		GameRegistry.addSmelting(Blocks.OBSIDIAN, new ItemStack(ModItems.OBSIDIAN_INGOT), 1.5f);
+		// Tools
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_AXE, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_PICKAXE, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_HOE, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_SHOVEL, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_SWORD, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		// Armor
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_BOOTS, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_CHESTPLATE, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_HELMET, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
+		GameRegistry.addSmelting(ModItems.OBSIDIAN_LEGGINGS, new ItemStack(ModItems.OBSIDIAN_NUGGET), ITEM_TO_NUGGET_EXP);
 	}
 }
